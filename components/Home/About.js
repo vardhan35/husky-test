82b71c5ff1292a4eccf21c0246953d37041bd2c7
vardhan35 <harshwardhankalse03@gmail.com>
2022-04-23 21:05:35 +0530

    test husky

--- components/Home/About.js
+++ components/Home/About.js
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import styles from "../../styles/About.module.css";
+import Link from 'next/link';
+import styles from '../../styles/About.module.css';
 
 function About() {
   return (
